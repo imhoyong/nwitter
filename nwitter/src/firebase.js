f5e1f35b4713d1116c3d6dcce047b634d7296abe
the_bible <the_bible@naver.com>
2021-12-22 10:29:46 +0900

    1.0 Initialization

---
+++ nwitter/src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/compat/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCH-EVxz9pOkr5N_WC6Z472zicSIUNykxw",
+    authDomain: "nwitter-9dadf.firebaseapp.com",
+    projectId: "nwitter-9dadf",
+    storageBucket: "nwitter-9dadf.appspot.com",
+    messagingSenderId: "710672543370",
+    appId: "1:710672543370:web:f07933b7bd6efe56b90773"
+  };
+
+    // Initialize Firebase
+export default  firebase.initializeApp(firebaseConfig);
